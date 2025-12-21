65192d0d1969856f3bd9db90c3ae242637b7676c
chakriswamireddy <iamthechakri@gmail.com>
2025-12-21 22:54:23 +0530

    fix - maxretries bullMQ err

--- src/config/queue.js
+++ src/config/queue.js
@@ -2,6 +2,8 @@
 import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
-const connection = new IORedis(process.env.REDIS_URL);
+export const connection = new IORedis(process.env.REDIS_URL, {
+    maxRetriesPerRequest: null,
+  });
 
 export const taskQueue = new Queue("tasks", { connection });
