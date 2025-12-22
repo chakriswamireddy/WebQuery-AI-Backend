0c97d37180ade92a149e21187a0205a3ddb3b9cc
chakriswamireddy <iamthechakri@gmail.com>
2025-12-22 10:51:40 +0530

    fix - worker not consume err

--- src/worker.js
+++ src/worker.js
@@ -8,25 +8,30 @@ import { redisConnection } from "./config/queue.js";
 import { db } from "./config/db.js";
 import { eq } from "drizzle-orm";
  
- 
+console.log("Worker process started");
+
 new Worker(
   "tasks",
   async (job) => {
+    console.log("Picked job", job.id);
+
     const { taskId, url, question } = job.data;
 
+    console.log("Updating status to processing");
     await db.update(tasks)
       .set({ status: "processing" })
       .where(eq(tasks.id, taskId));
 
+    console.log("Starting scrape");
     const content = await scrapeWebsite(url);
+
+    console.log("Scrape done, calling AI");
     const answer = await askAI(content, question);
 
+    console.log("Updating status to completed");
     await db.update(tasks)
-      .set({
-        status: "completed",
-        answer
-      })
+      .set({ status: "completed", answer })
       .where(eq(tasks.id, taskId));
   },
-  {connection:  redisConnection }
+  { connection: redisConnection }
 );
